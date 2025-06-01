951cf9e0543dbc5b95de46a459893ccd56dd6d02
Vidhi Arora <143219369+vidhi2919@users.noreply.github.com>
2025-06-01 05:51:22 +0530

    Update walletRoutes.js

--- routes/walletRoutes.js
+++ routes/walletRoutes.js
@@ -1,11 +1,3 @@
-// const router = require('express').Router();
-// const { createOrGetWallet, getAllWallets } = require('../controllers/walletController');
-// const { authenticate } = require('../middleware/auth');
-// router.post('/', createOrGetWallet);
-// router.get('/', getAllWallets);
-
-// module.exports = router;
-
 const router = require('express').Router();
 const { createOrGetWallet, getAllWallets} = require('../controllers/walletController');
 const { authenticate } = require('../middleware/auth');
