e979d0231c2ae78a0fdabe908721599684ed0bde
Vidhi Arora <143219369+vidhi2919@users.noreply.github.com>
2025-06-01 05:52:08 +0530

    Update dailyScan.js

--- cron/dailyScan.js
+++ cron/dailyScan.js
@@ -1,15 +1,3 @@
-// const cron = require('node-cron');
-// const Transaction = require('../models/Transaction');
-// const { sendAlertEmail } = require('../utils/email');
-
-// cron.schedule('0 0 * * *', async () => {
-//   const suspicious = await Transaction.find({ isFlagged: true });
-//   suspicious.forEach(txn => sendAlertEmail(txn));
-//   console.log('Daily fraud scan complete');
-// });
-
-// cron/dailyScan.js
-
 const cron = require('node-cron');
 const Transaction = require('../models/Transaction');
 const FraudScan = require('../models/FraudScan');
