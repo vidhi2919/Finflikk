37be0f2288be49a20565660650937326be0595f3
Vidhi Arora <143219369+vidhi2919@users.noreply.github.com>
2025-06-01 05:51:08 +0530

    Update transactionRoutes.js

--- routes/transactionRoutes.js
+++ routes/transactionRoutes.js
@@ -1,12 +1,3 @@
-// const router = require('express').Router();
-// const { deposit, withdraw, transfer } = require('../controllers/transactionController');
-
-// router.post('/deposit', deposit);
-// router.post('/withdraw', withdraw);
-// router.post('/transfer', transfer);
-
-// module.exports = router;
-
 const router = require('express').Router();
 const { deposit, withdraw, transfer , softDeleteTransaction} = require('../controllers/transactionController');
 const { authenticate } = require('../middleware/auth');
